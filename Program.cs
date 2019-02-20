ba976d71efa7ffb32bb86e176c2ff8fae398483d
Daniel Alpatov <danial.alpatov@gmail.com>
2019-02-20 18:28:43 +0300

    Немного изменён ход работы + добавлен текст Readme.md

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Events
+﻿namespace Events
 {
     class Program
     {
@@ -17,65 +15,8 @@ namespace Events
             }
         }
 
-        /// <summary>
-        ///     Аргументы события изменения имени.
-        /// </summary>
-        class NameChangeEventArgs : EventArgs
-        {
-            /// <summary>
-            ///     Имя до события.
-            /// </summary>
-            public string Old { get; }
-            
-            /// <summary>
-            ///     Имя, после события.
-            /// </summary>
-            public string Current { get; }
-
-            public NameChangeEventArgs(string old, string current)
-            {
-                Old = old;
-                Current = current;
-            }
-        }
-        
-        /// <summary>
-        ///     Представление о человеке
-        /// </summary>
-        class Person
-        {
-            /// <summary>
-            ///     Переменная, для хранения имени человека
-            /// </summary>
-            private string _name;
-            /// <summary>
-            ///     Свойство, обеспечивающее доступ к инкапсулированому имени.
-            /// </summary>
-            public string Name
-            {
-                get { return _name; }
-                set
-                {
-                    var args = new NameChangeEventArgs(_name, value);
-                    _name = value;
-                    if (NameChanged != null)
-                    {
-                        NameChanged(this, args);
-                    }
-                }
-            }
-            /// <summary>
-            ///     Событие, сигнализирубщее о том, что имя человека было изменено.
-            /// </summary>
-            public event EventHandler<NameChangeEventArgs> NameChanged;
-           
-            // ...
-        }
-
         static void Main(string[] args)
         {
-            Person person = new Person();
-            person.NameChanged += OnPersonNameChanged;
             Product product = new Product("Some product name", 0);
             
             /* 
@@ -86,19 +27,6 @@ namespace Events
              * TODO #7 Выполнить с экземпляром класса Product действия, 
              * приводящие к возникновению описанных Вами событий
              */
-        }
-
-        /// <summary>
-        ///     Обработчик события изменения имени человека
-        /// </summary>
-        private static void OnPersonNameChanged(object sender, NameChangeEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        static void OnHandleEvent(object sender, EventArgs e)
-        {
-            
         }
 
         /* 
