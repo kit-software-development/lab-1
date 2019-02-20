22f10fe62d0443440ce3976bf2dfcf81e7f1a1dd
Dmitriy Drobotov <dmitriy.drobotov@studiomobile.ru>
2019-02-20 20:32:36 +0300

    Solve task

---
+++ ProductChangedEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Events
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Класс, который служит для передачи аргументов 
+    /// в обработчик событий, возникающих в классе 
+    /// <seealso cref="T:Events.Product">Product</seealso>
+    /// </summary>
+    public class ProductChangedEventArgs<T> : EventArgs
+    {
+        public T OldValue { get; }
+
+        public T NewValue { get; }
+
+        public ProductChangedEventArgs(T oldValue, T newValue)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+    }
+}
