0c20e9feef6a7f9cef589547a9c525564e6b73d2
Herman Islamov <33521824+DGSR@users.noreply.github.com>
2019-02-26 16:28:46 +0300

    Add files via upload

--- ProductEventArgs.cs
+++ ProductEventArgs.cs
@@ -8,11 +8,46 @@
     /*
      * TODO #1 Закончить определение класса ProductEventArgs
      */
+   
     class ProductEventArgs
     {
         /* 
          * TODO #2 Добавить определение необходимых компонент 
          * класса ProductEventArgs
          */
+        public static readonly ProductEventArgs Empty = new ProductEventArgs();
+        public ProductEventArgs() { }
+    }
+    class NameChangeEventArgs :  ProductEventArgs {
+        /// <summary>
+        ///     Имя до события.
+        /// </summary>
+        public string Old { get; }
+
+        /// <summary>
+        ///     Имя, после события.
+        /// </summary>
+        public string Current { get; }
+
+        public NameChangeEventArgs(string old, string current)
+        {
+            Old = old;
+            Current = current;
+        }
+    }
+    class PriceChangeEventArgs: ProductEventArgs
+    {
+        public decimal Old { get; }
+
+        /// <summary>
+        ///     Имя, после события.
+        /// </summary>
+        public decimal Current { get; }
+
+        public PriceChangeEventArgs(decimal old, decimal current)
+        {
+            Old = old;
+            Current = current;
+        }
     }
 }
