6af404fcf16ebdf0cbd289c4c04f2ebb45f1f656
Daria Divenkova <47813973+dardiv@users.noreply.github.com>
2019-02-26 21:50:55 +0300

    Complete task

--- Program.cs
+++ Program.cs
@@ -1,24 +1,15 @@
-﻿namespace Events
+﻿using System;
+
+
+namespace Events
 {
     class Program
     {
-        internal Product Product
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-                throw new System.NotImplementedException();
-            }
-        }
-
+  
+        
         static void Main(string[] args)
         {
-            Product product = new Product("Some product name", 0);
-            
+                       
             /* 
              * TODO #6 Назначить обработчики событий в текущем контексте 
              */
@@ -27,11 +18,28 @@
              * TODO #7 Выполнить с экземпляром класса Product действия, 
              * приводящие к возникновению описанных Вами событий
              */
+
+            Product product = new Product("Some product name", 0);
+            product.NameChanged += OnNameChanged;
+            product.PriceChanged += OnPriceChanged;
+            product.Name = "Холодильник";
+            product.Price = 10000;
+
         }
 
         /* 
          * TODO #8 Добавить определение обработчиков событий 
          */
+
+        private static void OnNameChanged (object sender, ProductEventArgs<string> args)
+        {
+            Console.WriteLine("Название товара '{0}' изменилось на '{1}'", args.Oldvalue, args.Newvalue);
+        }
+
+        private static void OnPriceChanged(object sender, ProductEventArgs<decimal> args)
+        {
+            Console.WriteLine("Старая цена товара {0} равная {1} была изменена на {2}", ((Product) sender).Name, args.Oldvalue, args.Newvalue);
+        }
 
     }
 }
