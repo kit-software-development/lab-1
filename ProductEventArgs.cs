cc6fa75af8b75127c7736ad0360c1d9d3bd811ef
GregBold <37800579+GregBold@users.noreply.github.com>
2019-02-28 22:56:07 +0300

    Add files via upload

--- ProductEventArgs.cs
+++ ProductEventArgs.cs
@@ -1,18 +1,26 @@
-﻿namespace Events
+﻿using System;
+
+namespace Events
 {
     /// <summary>
     /// Класс, который служит для передачи аргументов 
     /// в обработчик событий, возникающих в классе 
     /// <seealso cref="Product">Product</seealso>
     /// </summary>
-    /*
-     * TODO #1 Закончить определение класса ProductEventArgs
-     */
-    class ProductEventArgs
+    /*TODO #1 Закончить определение класса ProductEventArgs*/
+    class ProductEventArgs : EventArgs
     {
-        /* 
-         * TODO #2 Добавить определение необходимых компонент 
-         * класса ProductEventArgs
-         */
+        public string Prev_Name { get; }
+        public string New_Name { get; }
+        public decimal Prev_Price { get; }
+        public decimal New_Price { get; }
+        /* TODO #2 Добавить определение необходимых компонент класса ProductEventArgs*/
+        public ProductEventArgs(string prev_Name, string new_Name, decimal prev_Price, decimal new_Price)
+        {
+            Prev_Name = prev_Name;
+            New_Name = new_Name;
+            Prev_Price = prev_Price;
+            New_Price = new_Price;
+        }
     }
 }
