feffef8ea257a271ca633e21f14499840c754840
dashapervushina <47885523+dashapervushina@users.noreply.github.com>
2019-03-01 10:19:07 +0300

    Complete task

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Events
+﻿using System;
+    
+namespace Events
 {
     class Program
     {
@@ -23,15 +25,31 @@
              * TODO #6 Назначить обработчики событий в текущем контексте 
              */
 
+            product.NameChanged += OnProductNameChanged;
+            product.PriceChanged += OnProductPriceChanged;
+
             /*
              * TODO #7 Выполнить с экземпляром класса Product действия, 
              * приводящие к возникновению описанных Вами событий
              */
+
+            product.Name = "Книга";
+            product.Price = 420;
+            Console.Read();
         }
 
         /* 
          * TODO #8 Добавить определение обработчиков событий 
          */
+      
+        private static void OnProductNameChanged(object sender, NameChangeEventArgs args)
+        {
+            Console.WriteLine("Наименование {0} изменено на {1}", args.Old, args.Current);
+        }
 
+        private static void OnProductPriceChanged(object sender, PriceChangeEventArgs args)
+        {
+            Console.WriteLine("Цена {0} изменена на {1}", args.Old, args.Current);
+        }
     }
 }
