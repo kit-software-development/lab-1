5e90b67f72ea646d234fea175c9307069cce60d6
EkaterinaAntipova <ekaterina.antipova.99@mail.ru>
2019-03-06 11:03:58 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -22,16 +22,33 @@
             /* 
              * TODO #6 Назначить обработчики событий в текущем контексте 
              */
+            product.NameChanged += ProductNameChanged;
+            product.PriceChanged += ProductPriceChanged;
 
             /*
              * TODO #7 Выполнить с экземпляром класса Product действия, 
              * приводящие к возникновению описанных Вами событий
              */
+            product.Name = "Product1";
+            product.Name = "NewProduct";
+            product.Price = 5677;
+            product.Price = 678;
         }
 
         /* 
          * TODO #8 Добавить определение обработчиков событий 
          */
+        private static void ProductNameChanged(object sender, ProductEventArgs e)
+        {
+            Product product = sender as Product;
+            Console.WriteLine("Name of {0} has changed to {1}", e.Prev_Name, e.New_Name);
+        }
+
+        private static void ProductPriceChanged(object sender, ProductEventArgs e)
+        {
+            Product product = sender as Product;
+            Console.WriteLine("Price has changed from {0} to {1}", e.Prev_Price, e.New_Price);
+        }
 
     }
 }
