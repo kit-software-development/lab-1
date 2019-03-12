03c65d33594d0ec47f6f545a2faf0ef675251b4e
ksemya <47814139+ksemya@users.noreply.github.com>
2019-03-13 01:13:25 +0300

    Update ProductEventArgs.cs

--- ProductEventArgs.cs
+++ ProductEventArgs.cs
@@ -1,18 +1,24 @@
-﻿namespace Events
+sing System;
+
+namespace Events
 {
     /// <summary>
     /// Класс, который служит для передачи аргументов 
     /// в обработчик событий, возникающих в классе 
     /// <seealso cref="Product">Product</seealso>
     /// </summary>
-    /*
-     * TODO #1 Закончить определение класса ProductEventArgs
-     */
-    class ProductEventArgs
+    
+    class ProductChangedEventArgs<T> : EventArgs
     {
-        /* 
-         * TODO #2 Добавить определение необходимых компонент 
-         * класса ProductEventArgs
-         */
+        public T OldValue { get; internal set; }
+
+        public T NewValue { get; internal set; }
+
+        public ProductChangedEventArgs(T oldValue, T newValue)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
     }
 }
+
