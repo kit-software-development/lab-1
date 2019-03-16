2c862485fdb427cff4d9662faa8a7d93e653ca66
VerKse <ksenofontovs.cs@gmail.com>
2019-03-16 14:17:10 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Events
+namespace Events
 {
     class Program
     {
@@ -18,20 +18,35 @@
         static void Main(string[] args)
         {
             Product product = new Product("Some product name", 0);
-            
+
             /* 
              * TODO #6 Назначить обработчики событий в текущем контексте 
              */
+            product.NameChangeEvent += OneProductNameChanged;
+            product.PriceChangeEvent += OneProductPriceChanged;
+
 
             /*
              * TODO #7 Выполнить с экземпляром класса Product действия, 
              * приводящие к возникновению описанных Вами событий
              */
+             product.Name = "New name";
+             product.Price = 1234;
         }
 
         /* 
          * TODO #8 Добавить определение обработчиков событий 
          */
+
+        private static void OneProductNameChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void OneProductPriceChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
 
     }
 }
